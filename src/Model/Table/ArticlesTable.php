d79f64a525da6fe2bee6d6e04800e75c40683f3e
gross1989 <tob.gross@gmail.com>
2018-08-15 16:32:06 +0200

    part 1

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,37 @@
+<?php
+// src/Model/Table/ArticlesTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\Utility\Text;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->notEmpty('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmpty('body')
+            ->minLength('body', 10);
+
+        return $validator;
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+}
